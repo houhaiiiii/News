e43fb54be9a66338b7c04d0a6b4b6c4fe2357fef
houhaiiiii <524668552@qq.com>
2020-12-28 21:47:12 +0800

    Day03 网管gateway完成未测试

---
+++ newsnews-admin-gateway/src/main/java/com/light/admin/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.light.admin.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * gateway网关模块引导类
+ * @author houhai
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class);
+    }
+
+}
