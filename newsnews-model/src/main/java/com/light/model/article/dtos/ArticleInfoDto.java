bd096cda9248bb9ada25153635c230054564a90a
houhaiiiii <524668552@qq.com>
2021-01-09 23:17:40 +0800

    Day09 APP端登录功能实现完成，未测试

--- newsnews-model/src/main/java/com/light/model/article/dtos/ArticleInfoDto.java
+++ newsnews-model/src/main/java/com/light/model/article/dtos/ArticleInfoDto.java
@@ -1,11 +1,13 @@
 package com.light.model.article.dtos;
 
+import com.light.model.common.annotation.IdEncrypt;
 import lombok.Data;
 
 @Data
 public class ArticleInfoDto {
 
     // 文章ID
+    @IdEncrypt
     Long articleId;
 
 }
